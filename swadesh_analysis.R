5adaf1514aba4026bfcbb92fa5ae836db8e3a21d
Brooke Hamilton <brookehamilton@gmail.com>
2018-02-19 10:14:08 -0500

    Updated edit_distance.ipynb

---
+++ swadesh_analysis.R
@@ -0,0 +1,10 @@
+## Linguistics 
+## 2018-02-18
+
+## Read in datasets
+# Meta data on all languages
+languages <- read.csv("/Users/brookehamilton/Dropbox/Rochester/linguistics/swadesh/Languages.csv")
+
+# Individual language wordlists
+
+test <- read.csv("/Users/brookehamilton/Downloads/asjp_dataset.cldf/asjp-contribution-CASUARINA_COAST_ASMAT.csv")
